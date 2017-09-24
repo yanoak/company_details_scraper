e14ea79e5cae9c9b2b0ab30f20e8c9cba50edce6
Nyan Lynn Myint <nyanlynn.razi@gmail.com>
2017-09-24 15:55:56 +0630

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,81 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import os
+from time import sleep
+from random import randint
+import sqlite3 as sql
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+links = pd.read_csv('dica_links.csv', header=None)
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+for url in links[0]:
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "lxml")
+    t1 = soup.find_all('div', attrs={'class':'field-items'})
+    t1_data = [x.findChildren(text=True) for x in t1]    
+    t2 = soup.find_all('td')
+    t2_data = [y.findChildren(text=True) for y in t2]
+    t2_data = [[u"".join(a).strip() for a in z]for z in t2_data]
+    records = []
+
+    name = None
+    name_mm = None
+    reg_no = None
+    reg_date = None
+    exp_date = None
+    addr = None
+
+    try:
+        name = t1_data[0]
+        name_mm = t1_data[1]
+        reg_no = t1_data[2]
+        reg_date = t1_data[3]
+        exp_date = t1_data[4]
+        addr = t1_data[5]
+    except:
+        pass
+
+
+    #Need to Clean up Variables due to Serious data inconsistency in the tables
+    dname_1 = None
+    dtype_1 = None
+    dnrc_1 = None
+    dname_2 = None
+    dtype_2 = None
+    dnrc_2 = None
+    dname_3 = None
+    dtype_3 = None
+    dnrc_3 = None
+
+
+
+    #try except to avoid Index Error
+    try:
+        dname_1 = t2_data[0]
+        dtype_1 = t2_data[1]
+        dnrc_1 = t2_data[2]
+        dname_2 = t2_data[5]
+        dtype_2 = t2_data[6]
+        dnrc_2 = t2_data[7]
+        dname_3 = t2_data[10]
+        dtype_3 = t2_data[11]
+        dnrc_3 = t2_data[12]
+    except:
+        pass
+
+    records.append((name, name_mm, reg_no, reg_date,exp_date,addr,dname_1,dtype_1,dnrc_1, dname_2, dtype_2, dnrc_2,dname_3, dtype_3,dnrc_3))
+
+
+
+    df = pd.DataFrame(records, columns=['name', 'name_mm', 'reg_no', 'reg_date','exp_date','addr','dname_1','dtype_1','dnrc_1', 'dname_2', 'dtype_2', 'dnrc_2','dname_3', 'dtype_3','dnrc_3'])
+
+
+    sql_con = sql.connect("data.sqlite")
+    df.to_sql(name='data',con=sql_con ,if_exists='append')
+    for _ in range(0,3): #to control the crawl rate
+        print('.')
+        sleep(randint(1,3))
+    
+
+
+
